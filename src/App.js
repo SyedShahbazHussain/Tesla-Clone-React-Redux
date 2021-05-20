f726b855eb5590cd4c9a64610d39ac840340c4ba
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2021-05-20 23:29:44 +0500

    Remove necessary imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import  Header  from './components/Header';
+// import  Header  from './components/Header';
 import './App.css';
 import Home from './components/Home';
-import styled, {  } from "styled-components";
 
 function App() {
   return (
