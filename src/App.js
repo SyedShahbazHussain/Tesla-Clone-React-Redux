c3e0e2e135abc5dd5b854a580ac7e32fa9dfeb71
SyedShahbazHussain <syedshahbaz.hussain@outlook.com>
2021-05-20 23:17:33 +0500

    Creating home sections

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import  Header  from './components/Header';
 import './App.css';
+import Home from './components/Home';
+import styled, {  } from "styled-components";
 
 function App() {
   return (
     <div className="App">
+      {/* <Header/> */}
+      <Home/>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components";
+
+
+function Header() {
+    return (
+        <Container>
+            header
+        </Container>
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+
+`
